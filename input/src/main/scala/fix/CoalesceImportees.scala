7a265e65676549456d373f01b344bf65705de001
Cheng Lian <lian@databricks.com>
2020-06-27 02:21:38 -0700

    Fix Scala 2.13 compilation error

--- input/src/main/scala/fix/CoalesceImportees.scala
+++ input/src/main/scala/fix/CoalesceImportees.scala
@@ -9,7 +9,7 @@ package fix
 
 import scala.collection.immutable.{Seq, Map, Vector}
 import scala.collection.mutable.{Buffer, Seq, Map, Set}
-import scala.concurrent.{Lock, Await, Channel => Ch, Future, Promise}
+import scala.concurrent.{Await, Channel => Ch, Future, Promise, duration}
 import scala.util.{Either, Random => _, Try, Success, Failure}
 
 object CoalesceImportees
