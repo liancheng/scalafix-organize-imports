8977dcad1e7fee08a32daec7921f8c1f294d419f
Cheng Lian <lian@databricks.com>
2021-01-15 12:04:12 -0800

    Add ConfEncoders for all configurations

--- rules/src/main/scala/fix/OrganizeImportsConfig.scala
+++ rules/src/main/scala/fix/OrganizeImportsConfig.scala
@@ -1,8 +1,11 @@
 package fix
 
+import metaconfig.Conf
 import metaconfig.ConfDecoder
+import metaconfig.ConfEncoder
 import metaconfig.generic.Surface
 import metaconfig.generic.deriveDecoder
+import metaconfig.generic.deriveEncoder
 import metaconfig.generic.deriveSurface
 import scalafix.internal.config.ReaderUtil
 
@@ -13,9 +16,8 @@ object ImportsOrder {
   case object SymbolsFirst extends ImportsOrder
   case object Keep extends ImportsOrder
 
-  implicit def reader: ConfDecoder[ImportsOrder] = ReaderUtil.fromMap(
-    (List(Ascii, SymbolsFirst, Keep) map (v => v.toString -> v)).toMap
-  )
+  implicit def reader: ConfDecoder[ImportsOrder] = ReaderUtil.oneOf(Ascii, SymbolsFirst, Keep)
+  implicit def writer: ConfEncoder[ImportsOrder] = ConfEncoder.instance(v => Conf.Str(v.toString))
 }
 
 sealed trait ImportSelectorsOrder
@@ -25,9 +27,11 @@ object ImportSelectorsOrder {
   case object SymbolsFirst extends ImportSelectorsOrder
   case object Keep extends ImportSelectorsOrder
 
-  implicit def reader: ConfDecoder[ImportSelectorsOrder] = ReaderUtil.fromMap {
-    (List(Ascii, SymbolsFirst, Keep) map (v => v.toString -> v)).toMap
-  }
+  implicit def reader: ConfDecoder[ImportSelectorsOrder] =
+    ReaderUtil.oneOf(Ascii, SymbolsFirst, Keep)
+
+  implicit def writer: ConfEncoder[ImportSelectorsOrder] =
+    ConfEncoder.instance(v => Conf.Str(v.toString))
 }
 
 sealed trait GroupedImports
@@ -38,9 +42,11 @@ object GroupedImports {
   case object Explode extends GroupedImports
   case object Keep extends GroupedImports
 
-  implicit def reader: ConfDecoder[GroupedImports] = ReaderUtil.fromMap {
-    (List(AggressiveMerge, Merge, Explode, Keep) map (v => v.toString -> v)).toMap
-  }
+  implicit def reader: ConfDecoder[GroupedImports] =
+    ReaderUtil.oneOf(AggressiveMerge, Merge, Explode, Keep)
+
+  implicit def writer: ConfEncoder[GroupedImports] =
+    ConfEncoder.instance(v => Conf.Str(v.toString))
 }
 
 sealed trait BlankLines
@@ -49,24 +55,21 @@ object BlankLines {
   case object Auto extends BlankLines
   case object Manual extends BlankLines
 
-  implicit def reader: ConfDecoder[BlankLines] = ReaderUtil.fromMap {
-    (List(Auto, Manual) map (v => v.toString -> v)).toMap
-  }
+  implicit def reader: ConfDecoder[BlankLines] = ReaderUtil.oneOf(Auto, Manual)
+  implicit def writer: ConfEncoder[BlankLines] = ConfEncoder.instance(v => Conf.Str(v.toString))
 }
 
-sealed trait BaseOnStyle
+sealed trait Preset
 
-object BaseOnStyle {
-  case object Default extends BaseOnStyle
-  case object IntelliJ extends BaseOnStyle
+object Preset {
+  case object DEFAULT extends Preset
+  case object INTELLIJ_2020_3 extends Preset
 
-  implicit def reader: ConfDecoder[BaseOnStyle] = ReaderUtil.fromMap {
-    (List(Default, IntelliJ) map (v => v.toString -> v)).toMap
-  }
+  implicit def reader: ConfDecoder[Preset] = ReaderUtil.oneOf(DEFAULT, INTELLIJ_2020_3)
+  implicit def writer: ConfEncoder[Preset] = ConfEncoder.instance(v => Conf.Str(v.toString))
 }
 
 final case class OrganizeImportsConfig(
-  baseOnStyle: BaseOnStyle = BaseOnStyle.Default,
   blankLines: BlankLines = BlankLines.Auto,
   coalesceToWildcardImportThreshold: Int = Int.MaxValue,
   expandRelative: Boolean = false,
@@ -79,6 +82,7 @@ final case class OrganizeImportsConfig(
   ),
   importSelectorsOrder: ImportSelectorsOrder = ImportSelectorsOrder.Ascii,
   importsOrder: ImportsOrder = ImportsOrder.Ascii,
+  preset: Preset = Preset.DEFAULT,
   removeUnused: Boolean = true
 )
 
@@ -91,9 +95,12 @@ object OrganizeImportsConfig {
   implicit val decoder: ConfDecoder[OrganizeImportsConfig] =
     deriveDecoder[OrganizeImportsConfig](default)
 
-  val styles: Map[BaseOnStyle, OrganizeImportsConfig] = Map(
-    BaseOnStyle.Default -> OrganizeImportsConfig(),
-    BaseOnStyle.IntelliJ -> OrganizeImportsConfig(
+  implicit val encoder: ConfEncoder[OrganizeImportsConfig] =
+    deriveEncoder[OrganizeImportsConfig]
+
+  val presets: Map[Preset, OrganizeImportsConfig] = Map(
+    Preset.DEFAULT -> OrganizeImportsConfig(),
+    Preset.INTELLIJ_2020_3 -> OrganizeImportsConfig(
       blankLines = BlankLines.Manual,
       groupedImports = GroupedImports.Merge,
       groups = Seq(
