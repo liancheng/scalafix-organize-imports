21dbc14835e3f59eafdccc7ab6ba424ffbf96708
Cheng Lian <lian@databricks.com>
2021-08-18 00:43:59 -0700

    Refactor for simplicity

--- input/src/main/scala-3/fix/DeduplicateGivenImportees.scala
+++ input/src/main/scala-3/fix/DeduplicateGivenImportees.scala
@@ -6,8 +6,8 @@ package fix
 
 import fix.Givens._
 import fix.Givens.{given A, given B}
-import fix.Givens.{given A}
-import fix.Givens.{given C}
-import fix.Givens.{given C}
+import fix.Givens.given A
+import fix.Givens.given C
+import fix.Givens.given C
 
 object DeduplicateGivenImportees
